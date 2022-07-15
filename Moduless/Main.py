b47910c502132e9d3b7c1d03c0efc56b31ac44fc
Fayik <veznedaroglufayik2@gmail.com>
2022-07-16 00:21:05 +0300

    sa

---
+++ Moduless/Main.py
@@ -0,0 +1,12 @@
+import mod
+
+result=mod.number
+result=mod.person["name"]
+result=mod.person["age"]
+result1=mod.numbers
+
+
+print(result)
+print(result1)
+
+
---
+++ Moduless/mod.py
@@ -0,0 +1,20 @@
+
+print("modül eklendi")
+
+number = 10
+numbers=[1,2,3]
+
+person={
+    "name":"Ali",
+    "age":"25",
+    "city":"İstanbul"
+}
+
+def func(x):
+    print(f'x: {x}')
+
+
+
+class Person:
+    def speak(Self):
+        print("I am speaking")
